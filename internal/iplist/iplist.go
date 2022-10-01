07c951069f78ff0cc9f6e6499f3bd37948161607
beauknowstech <git@beaugraham.com>
2022-10-01 01:33:59 -0400

    cleanup

---
+++ internal/iplist/iplist.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net"
+	"os"
+)
+
+func main() {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			os.Stdout.WriteString(ipnet.IP.String() + "\n")
+		}
+	}
+}
--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"gup/internal/filelist"
 	"net/http"
 	"strconv"
+
+	"github.com/fatih/color"
 )
 
 func main() {
 
-	recursive := flag.Bool("r", false, "Recursive or nah?")
 	webroot := flag.String("d", ".", "Specify which directory")
 	port := flag.Int("p", 80, "Specify which port to listen on")
+	recursive := flag.Bool("r", false, "Recursive or nah?")
 	flag.Parse()
 
 	// create file server handler
@@ -23,18 +25,24 @@ func main() {
 	//}
 	strport := ":" + (strconv.Itoa(*port))
 
-	// start HTTP server with `fs` as the default handler
-	fmt.Printf("Directory: %s\nPort: %d\n", *webroot, *port)
+	bold := color.New(color.Bold, color.Underline)
+	//print info
+	//bold.Printf("Directory: %s\nPort: %d\n", *webroot, *port)
+	bold.Print("Directory:")
+	fmt.Print(" " + *webroot)
+	bold.Print("\nPort:")
+	fmt.Print(" " + (strconv.Itoa(*port)))
+
 	// print list of files
 
 	if *recursive {
-		fmt.Print("Files:\n")
+		bold.Print("\nFiles:\n")
 		filelist.ListFilesrecursive(*webroot)
 	} else {
-		fmt.Print("Files:\n")
+		bold.Print("\nFiles:\n")
 		filelist.ListFiles(*webroot)
 	}
-
+	// start HTTP server with `fs` as the default handler
 	fmt.Println(http.ListenAndServe(strport, fs))
 
 }
